7fcb545316f7557dc52a5e6dbfe192ce2d573b86
Ernst Widerberg <ernstwi@kth.se>
2021-08-16 16:01:18 +0200

    Simplify verify-setup.sh

--- verify-setup.sh
+++ verify-setup.sh
@@ -1,11 +1,11 @@
-docker exec revoke_jwt_1 cat /etc/nginx/conf.d/snakeoil.crt > /tmp/jwt.crt
-docker exec revoke_jwt_1 cat /etc/nginx/conf.d/snakeoil.key > /tmp/jwt.key
+docker cp revoke_jwt_1:/etc/nginx/conf.d/snakeoil.crt /tmp/jwt.crt
+docker cp revoke_jwt_1:/etc/nginx/conf.d/snakeoil.key /tmp/jwt.key
 cmp jwt.crt /tmp/jwt.crt
 cmp jwt.key /tmp/jwt.key
 
-docker exec revoke_jwt_1 cat /opt/auth-server-poc/cert/public.pem > /tmp/jwt-sign.pem
-docker exec revoke_back_1 cat /usr/src/app/certs/jwt.pem > /tmp/back-jwt-sign.pem
+docker cp revoke_jwt_1:/opt/auth-server-poc/cert/public.pem /tmp/jwt-sign.pem
+docker cp revoke_back_1:/usr/src/app/certs/jwt.pem /tmp/back-jwt-sign.pem
 cmp /tmp/jwt-sign.pem /tmp/back-jwt-sign.pem
 
-docker exec revoke_jwt_1 cat /opt/auth-server-poc/userdb/.htpasswd > /tmp/jwt-htpasswd
+docker cp revoke_jwt_1:/opt/auth-server-poc/userdb/.htpasswd /tmp/jwt-htpasswd
 cmp /tmp/jwt-htpasswd htpasswd
